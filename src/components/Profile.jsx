6d52051ef57b95785920274e97f979fb22c8b77c
Aleksandr Datsiura <aleksandrdatsiura@gmail.com>
2021-06-08 14:14:39 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React, { Profiler } from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/f/f7/Melbourne_Docklands_-_Yarras_Edge_-_marina_panorama.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
